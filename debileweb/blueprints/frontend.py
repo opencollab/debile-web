fba35be1fa32f1d5409802def36f6e1edde7526f
Léo Cavaillé <leo@cavaille.net>
2013-08-26 14:18:35 +0200

    Flatten directory hierarchy

--- debileweb/blueprints/frontend.py
+++ debileweb/blueprints/frontend.py
@@ -20,9 +20,9 @@
 
 from flask import Blueprint, render_template, send_file
 from sqlalchemy.orm import joinedload
-from debile.master.orm import Package, Source, Binary, Machine, User, Job, Group
-from debile.master.config import Config
-from debile.master.server import Session
+from debilemaster.orm import Package, Source, Binary, Machine, User, Job, Group
+from debilemaster.config import Config
+from debilemaster.server import Session
 
 from humanize import naturaltime
 from humanize.time import naturaldelta
