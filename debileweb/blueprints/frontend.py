e00344f49ef7707b53ce559c2f699c1f1d80c163
Sylvestre Ledru <sylvestre@debian.org>
2013-08-28 16:39:50 +0200

    add the copyright

--- debileweb/blueprints/frontend.py
+++ debileweb/blueprints/frontend.py
@@ -1,4 +1,6 @@
 # Copyright (c) 2012 Paul Tagliamonte <paultag@debian.org>
+# Copyright (c) 2013 Leo Cavaille <leo@cavaille.net>
+# Copyright (c) 2013 Sylvestre Ledru <sylvestre@debian.org>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
